3d16510615a5e3265f9874d573aa73ac4fc590b3
Ingridtsp <ingridnicole@usf.edu>
2025-11-23 15:52:48 -0500

    importing expense db

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
+// App.js
 import React from 'react';
 import { SQLiteProvider } from 'expo-sqlite';
+import ExpenseScreen from './ExpenseScreen';
 
 export default function App() {
   return (
     <SQLiteProvider databaseName="expenses.db">
-      <main />
+      <ExpenseScreen />
     </SQLiteProvider>
   );
 }
